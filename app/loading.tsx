bd161732c129db71b48dd2a1e6d7548e51f01501
Shivamgarg581 <shivgarg184@gmail.com>
2026-09-22 13:35:14 +0530

    feat: add app/loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+export default function Loading() {
+  return (
+    <main className="void-screen">
+      <div className="void-copy">
+        <span className="micro-line">NO SIGNAL</span>
+        <div className="title-mark" aria-hidden="true">◇</div>
+        <p className="line-a">Finding the door.</p>
+        <p className="line-b">Please don't blink.</p>
+      </div>
+    </main>
+  );
+}
